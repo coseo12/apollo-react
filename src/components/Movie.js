22a7a838937f948a1f177cc1c8d09c6a785d2710
coseo12 <momo_12@naver.com>
2020-10-22 17:30:38 +0900

    add

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { gql, useMutation } from '@apollo/client';
 
 const Container = styled.div`
-  height: 380px;
+  height: 400px;
+  border-radius: 7px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   overflow: hidden;
-  border-radius: 7px;
+  background-color: transparent;
 `;
 const Poster = styled.div`
   background-image: url(${props => props.bg});
@@ -15,14 +17,27 @@ const Poster = styled.div`
   width: 100%;
   background-size: cover;
   background-position: center center;
+  border-radius: 7px;
 `;
 
-const Movie = ({ id, bg }) => {
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
+
+const Movie = ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
   return (
     <Container>
       <Link to={`/${id}`}>
         <Poster bg={bg} />
       </Link>
+      <button onClick={isLiked ? null : likeMovie}>
+        {isLiked ? 'Unlike' : 'Like'}
+      </button>
     </Container>
   );
 };
